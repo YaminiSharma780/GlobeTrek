b45cdcdcb14ed19048748e703a9850949c1118a9
Yamini <sharmayamini1999@gmail.com>
2025-02-17 01:18:12 +0530

    implemented search country functionality

--- App.jsx
+++ App.jsx
@@ -1,19 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import CountriesList from "./components/CountriesList";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import SelectRegion from "./components/SelectRegion";
 const App = () => {
+  const [query, setQuery] = useState("");
   return (
     <>
       <Header />
       <main>
         <div className="search-filter-container">
-          <SearchBar />
+          <SearchBar setQuery={setQuery} />
           <SelectRegion />
         </div>
         <div>
-            <CountriesList/>
+          <CountriesList query={query} />
         </div>
       </main>
     </>
--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import data from "../data";
 import CountryCard from "./CountryCard";
-export default function CountriesList() {
-  const arr = data.map((country) => {
-    return (
-      <CountryCard
-        key={country.name.common}
-        name={country.name.common}
-        flag={country.flags.svg}
-        population={country.population.toLocaleString("en-IN")}
-        region={country.region}
-        capital={country.capital?.[0]}
-      />
-    );
-  });
-  return <div className="countries-container">{arr}</div>;
+export default function CountriesList({ query }) {
+  return (
+    <>
+      <div className="countries-container">
+        {data
+          .filter((country) =>
+            country.name.common.toLowerCase().includes(query)
+          )
+          .map((country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                name={country.name.common}
+                flag={country.flags.svg}
+                population={country.population.toLocaleString("en-IN")}
+                region={country.region}
+                capital={country.capital?.[0]}
+              />
+            );
+          })}
+      </div>
+    </>
+  );
 }
