881e91a97d0093ee9628892a52035423ae25e781
Yamini <sharmayamini1999@gmail.com>
2025-02-21 02:21:49 +0530

    improved shimmer effect on the home page

--- components/CountriesListShimmer.jsx
+++ components/CountriesListShimmer.jsx
@@ -2,7 +2,15 @@ export default function CountriesListShimmer() {
   return (
     <div className="countries-container">
       {Array.from({ length: 10 }).map((element, index) => {
-        return <div key={index} className="country-card shimmer-card"></div>;
+        return (
+          <div key={index} className="country-card shimmer-card">
+            <div className="shimmer-img"></div>
+            <div className="shimmer-heading"></div>
+            <div className="shimmer-text"></div>
+            <div className="shimmer-text"></div>
+            <div className="shimmer-text"></div>
+          </div>
+        );
       })}
     </div>
   );
